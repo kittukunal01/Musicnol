6a277fc933f6c9cf9c2baa74775edcd699087750
blade_xo <89949214+bladexo@users.noreply.github.com>
2023-10-09 21:32:11 +0100

    Update slow.py

--- mbot/plugins/slow.py
+++ mbot/plugins/slow.py
@@ -206,6 +206,6 @@ async def search(Mbot: Mbot, query: CallbackQuery):
         try:
             await query.message.reply_text(f"Done✅",   
          reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Feedback", callback_data="feed")]]))
-            await query.message.reply_text(f"Check out @spotify_downloa (music)  @spotifynewss(News)")
+            await query.message.reply_text(f"Check out @musicdownloaderr")
         except:
             pass     
