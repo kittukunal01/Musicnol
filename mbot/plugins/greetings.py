9df31a3417acc19039e4b0f8c5a8b303502a538d
blade_xo <89949214+bladexo@users.noreply.github.com>
2023-10-09 21:11:24 +0100

    Update greetings.py

--- mbot/plugins/greetings.py
+++ mbot/plugins/greetings.py
@@ -32,15 +32,15 @@ from os import execvp,sys
 async def start(client,message):
     reply_markup = [[
         InlineKeyboardButton(
-            text="Bot Channel", url="https://t.me/Spotify_downloa"),
+            text="Bot Channel", url="https://t.me/musicdownloaderr"),
         InlineKeyboardButton(
             text="Repo",
-            url="https://github.com/Masterolic/Spotify-Downloader/"),
+            url="https://github.com/Masterolic/Spotify-Downloaer/"),
         InlineKeyboardButton(text="Help",callback_data="helphome")
         ],
         [
             InlineKeyboardButton(text="Donate",
-            url="https://www.buymeacoffee.com/Masterolic"),
+            url="https://www.buymeacoffee.com/davidwordsb"),
         ]]
     if LOG_GROUP:
 
@@ -99,5 +99,5 @@ async def help_home(_,query):
     button = [
         [InlineKeyboardButton(text=i, callback_data=f"help_{i}")] for i in HELP
     ]
-    await query.message.edit(f"Hello **{query.from_user.first_name}**, I'm **@NeedMusicRobot**.\nI'm Here to download your music.",
+    await query.message.edit(f"Hello **{query.from_user.first_name}**, I'm **@Music_mp33_bot**.\nI'm Here to download your music.",
                         reply_markup=InlineKeyboardMarkup(button))
