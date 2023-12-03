7b86fdd298e471ca3efda9180e8383d2d39d1b9e
Balapriyan B <131746104+BalaPriyan@users.noreply.github.com>
2023-12-03 10:22:03 +0530

    Update Shazam.py

--- mbot/plugins/Shazam.py
+++ mbot/plugins/Shazam.py
@@ -7,33 +7,33 @@ from pyrogram.types import Message , InlineKeyboardMarkup, InlineKeyboardButton 
 from pyrogram.errors import FloodWait 
 from asyncio import sleep
 #from database.users_chats_db import db
-#from utils import get_size
+from utils import get_size
 from shazamio import Shazam
-#import math
+import math
 import asyncio
 import time
-#import shlex
-#import aiofiles
-#import aiohttp
-#import wget
+import shlex
+import aiofiles
+import aiohttp
+import wget
 import os
-#from asgiref.sync import sync_to_async
+from asgiref.sync import sync_to_async
 from requests import get
 from mbot.utils.util import run_cmd as runcmd
 import datetime
 from json import JSONDecodeError
 import requests
-#import ffmpeg 
+import ffmpeg 
 from pyrogram.errors import FloodWait, MessageNotModified
 from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
 #from youtubesearchpython import VideosSearch
 import yt_dlp
-#from youtube_search import YoutubeSearch
+from youtube_search import YoutubeSearch
 import requests
 from typing import Tuple
 from pyrogram import filters
 from pyrogram import Client
-#from mbot import OWNER_ID as ADMINS
+from mbot import OWNER_ID as ADMINS
 import time
 from apscheduler.schedulers.background import BackgroundScheduler
 from mbot.utils.shazam import humanbytes, edit_or_reply, fetch_audio
